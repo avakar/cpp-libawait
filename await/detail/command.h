eccdeb00c347e78abf7b09a8f030701d4d8ebc51
Martin Vejnár <avakar@ratatanek.cz>
2016-05-21 23:48:49 +0200

    Exception safety.

--- await/detail/command.h
+++ await/detail/command.h
@@ -2,6 +2,7 @@
 #define AWAIT_DETAIL_COMMAND_H
 
 #include "../task.h"
+#include <exception>
 
 namespace aw {
 namespace detail {
@@ -19,80 +20,80 @@ aw::task<typename I::value_type> aw::detail::make_command(P &&... p)
 
 	task<T> t;
 
-	void * storage = detail::task_access::storage(t);
-	size_t size = detail::task_access::storage_size<T>();
-
-	if (sizeof(I) <= size)
+	try
 	{
-		struct impl
+		void * storage = detail::task_access::storage(t);
+		size_t size = detail::task_access::storage_size<T>();
+
+		if (sizeof(I) <= size)
 		{
-			static task<T> start(void * self, detail::scheduler & sch, detail::task_completion<T> & sink)
+			struct impl
 			{
-				return static_cast<I *>(self)->start(sch, sink);
-			}
+				static task<T> start(void * self, detail::scheduler & sch, detail::task_completion<T> & sink)
+				{
+					return static_cast<I *>(self)->start(sch, sink);
+				}
 
-			static void move_to(void * self, void * dst)
-			{
-				I * ss = static_cast<I *>(self);
-				new(dst) I(std::move(*ss));
-				ss->~I();
-			}
+				static void move_to(void * self, void * dst)
+				{
+					I * ss = static_cast<I *>(self);
+					new(dst) I(std::move(*ss));
+					ss->~I();
+				}
 
-			static void destroy(void * self)
-			{
-				static_cast<I *>(self)->~I();
-			}
-		};
+				static void destroy(void * self)
+				{
+					static_cast<I *>(self)->~I();
+				}
+			};
 
-		static detail::task_vtable<T> const vtable = {
-			&impl::start,
-			nullptr,
-			&impl::move_to,
-			&impl::destroy,
-		};
+			static detail::task_vtable<T> const vtable = {
+				&impl::start,
+				nullptr,
+				&impl::move_to,
+				&impl::destroy,
+			};
 
-		new(storage) I(std::forward<P>(p)...);
-		detail::task_access::set_vtable(t, &vtable);
-	}
-	else
-	{
-		struct impl
-		{
-			static task<T> start(void * self, detail::scheduler & sch, detail::task_completion<T> & sink)
-			{
-				return (*static_cast<I **>(self))->start(sch, sink);
-			}
-
-			static void move_to(void * self, void * dst)
-			{
-				I ** ss = static_cast<I **>(self);
-				new(dst) I *(*ss);
-			}
-
-			static void destroy(void * self)
-			{
-				I ** ss = static_cast<I **>(self);
-				delete ss;
-			}
-		};
-
-		static detail::task_vtable<T> const vtable = {
-			&impl::start,
-			nullptr,
-			&impl::move_to,
-			&impl::destroy,
-		};
-
-		I * ss = new(std::nothrow) I(std::forward<P>(p)...);
-		if (ss == nullptr)
-		{
-			t = std::make_exception_ptr(std::bad_alloc());
+			new(storage) I(std::forward<P>(p)...);
+			detail::task_access::set_vtable(t, &vtable);
 		}
 		else
 		{
+			struct impl
+			{
+				static task<T> start(void * self, detail::scheduler & sch, detail::task_completion<T> & sink)
+				{
+					return (*static_cast<I **>(self))->start(sch, sink);
+				}
+
+				static void move_to(void * self, void * dst)
+				{
+					I ** ss = static_cast<I **>(self);
+					new(dst) I *(*ss);
+				}
+
+				static void destroy(void * self)
+				{
+					I ** ss = static_cast<I **>(self);
+					delete ss;
+				}
+			};
+
+			static detail::task_vtable<T> const vtable = {
+				&impl::start,
+				nullptr,
+				&impl::move_to,
+				&impl::destroy,
+			};
+
+			I * ss = new I(std::forward<P>(p)...);
 			new(storage) I *(ss);
 			detail::task_access::set_vtable(t, &vtable);
 		}
+	}
+	catch (...)
+	{
+		t = std::current_exception();
 	}
 
 	return std::move(t);
