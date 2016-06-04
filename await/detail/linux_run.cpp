bbb089beda5594750d9057a0c64bb7bd994335ae
Martin Vejnár <avakar@ratatanek.cz>
2016-06-04 23:13:18 +0200

    Fix runner.

--- await/detail/linux_run.cpp
+++ await/detail/linux_run.cpp
@@ -1,5 +1,6 @@
 #include "../run.h"
 #include "linux_scheduler.h"
+#include "intrusive_list.h"
 #include <sys/epoll.h>
 #include <poll.h>
 #include <unistd.h>
@@ -7,6 +8,13 @@
 aw::result<void> aw::detail::try_run_impl(task<void> && t)
 {
 	assert(!t.empty());
+
+	struct epoll_item
+		: intrusive_hook
+	{
+		int fd;
+		scheduler::fd_completion_sink * sink;
+	};
 
 	struct fake_scheduler
 		: scheduler, task_completion<void>
@@ -24,12 +32,16 @@ aw::result<void> aw::detail::try_run_impl(task<void> && t)
 
 		void add_fd(int fd, short events, fd_completion_sink & sink) override
 		{
+			epoll_item * item = new epoll_item();
+			item->fd = fd;
+			item->sink = &sink;
+
 			epoll_event ee;
-			ee.events = EPOLLONESHOT;
-			if (events & POLLIN)
-				ee.events |= EPOLLIN;
-			ee.data.ptr = &sink;
+			ee.events = EPOLLONESHOT | (events & (POLLIN | POLLPRI | POLLOUT));
+			ee.data.ptr = item;
 			epoll_ctl(m_epoll, EPOLL_CTL_ADD, fd, &ee);
+
+			m_items.push_back(*item);
 		}
 
 		void on_completion(scheduler & sch, task<void> && t) override
@@ -42,6 +54,7 @@ aw::result<void> aw::detail::try_run_impl(task<void> && t)
 		}
 
 		task<void> & m_task;
+		intrusive_list<epoll_item> m_items;
 		bool m_updated;
 		int m_epoll;
 	};
@@ -67,7 +80,15 @@ aw::result<void> aw::detail::try_run_impl(task<void> && t)
 		{
 			epoll_event ee;
 			epoll_wait(sch.m_epoll, &ee, 1, -1);
-			((detail::scheduler::fd_completion_sink *)ee.data.ptr)->on_completion(sch, ee.events);
+
+			epoll_item * item = static_cast<epoll_item *>(ee.data.ptr);
+			scheduler::fd_completion_sink * sink = item->sink;
+
+			epoll_ctl(sch.m_epoll, EPOLL_CTL_DEL, item->fd, nullptr);
+			sch.m_items.remove(*item);
+			delete item;
+
+			sink->on_completion(sch, ee.events);
 		}
 
 		vtable = detail::task_access::get_vtable(t);
