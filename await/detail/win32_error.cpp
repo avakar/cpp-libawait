9a3d71464d03a344f5073f088adee472dc1d5cce
Martin Vejnár <avakar@ratatanek.cz>
2016-06-05 00:53:23 +0200

    Fix win32_error.

--- await/detail/win32_error.cpp
+++ await/detail/win32_error.cpp
@@ -1,18 +1,53 @@
 #include "win32_error.h"
+#include <utility>
 #include <windows.h>
 
 aw::detail::win32_error::win32_error(uint32_t ec)
 	: m_ec(ec)
 {
-	char buf[65536];
-	DWORD r = FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM, nullptr, ec, 0x0409, buf, sizeof buf, nullptr);
+	char * buf = nullptr;
+	DWORD r = FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_ALLOCATE_BUFFER, nullptr, ec, 0, (LPSTR)&buf, 0, nullptr);
 	if (r != 0)
-		m_msg.assign(buf, r);
+		m_msg = buf;
+}
+
+aw::detail::win32_error::win32_error(win32_error const & o)
+	: m_ec(o.m_ec), m_msg(nullptr)
+{
+	if (o.m_msg != nullptr)
+	{
+		size_t len = strlen(o.m_msg);
+		char * buf = (char *)LocalAlloc(0, len + 1);
+		if (buf != nullptr)
+		{
+			memcpy(buf, o.m_msg, len + 1);
+			m_msg = buf;
+		}
+	}
+}
+
+aw::detail::win32_error::win32_error(win32_error && o)
+	: m_ec(o.m_ec), m_msg(o.m_msg)
+{
+	o.m_msg = nullptr;
+}
+
+aw::detail::win32_error::~win32_error()
+{
+	if (m_msg)
+		LocalFree((HLOCAL)m_msg);
+}
+
+aw::detail::win32_error & aw::detail::win32_error::operator=(win32_error o)
+{
+	m_ec = o.m_ec;
+	std::swap(m_msg, o.m_msg);
+	return *this;
 }
 
 char const * aw::detail::win32_error::what() const
 {
-	return m_msg.empty()? "win32_error": m_msg.c_str();
+	return m_msg? m_msg: "win32_error";
 }
 
 uint32_t aw::detail::win32_error::errorcode() const
--- await/detail/win32_error.h
+++ await/detail/win32_error.h
@@ -3,7 +3,6 @@
 
 #include <exception>
 #include <stdint.h>
-#include <string>
 
 namespace aw {
 namespace detail {
@@ -12,13 +11,18 @@ struct win32_error
 	: std::exception
 {
 	explicit win32_error(uint32_t ec);
+	win32_error(win32_error const & o);
+	win32_error(win32_error && o);
+	~win32_error();
+
+	win32_error & operator=(win32_error o);
 
 	char const * what() const override;
 	uint32_t errorcode() const;
 
 private:
 	uint32_t m_ec;
-	std::string m_msg;
+	char const * m_msg;
 };
 
 }
