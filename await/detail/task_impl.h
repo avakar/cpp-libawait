1a2ff755d115fbb98fea09e304cec7a3b8a365f3
Martin Vejnár <avakar@ratatanek.cz>
2016-06-13 22:13:58 +0200

    Refactoring.

--- await/detail/task_impl.h
+++ await/detail/task_impl.h
@@ -1,4 +1,4 @@
-#include "task_access.h"
+#include "command_ptr.h"
 #include <assert.h>
 #include <new>
 #include <tuple>
@@ -23,6 +23,46 @@ task_kind construct_result<void, void>(void * storage, result<void> && v) noexce
 
 template <>
 task_kind construct_result<void, void>(void * storage, result<void> const & v) noexcept;
+
+template <typename T>
+void move_value(void * dst, void * src);
+
+template <>
+void move_value<void>(void * dst, void * src);
+
+template <typename T>
+void destroy_value(void * p);
+
+template <>
+void destroy_value<void>(void * p);
+
+template <typename T>
+result<T> dismiss_value(void * p);
+
+template <>
+result<void> dismiss_value<void>(void * p);
+
+template <typename T>
+void move_task(task<T> & dst, task<T> & src);
+
+template <typename T>
+bool has_command(task<T> const & t);
+
+template <typename T>
+command_ptr<T> fetch_command(task<T> & t);
+
+template <typename T>
+bool start_command(task<T> & t, scheduler & sch, task_completion<T> & sink);
+
+template <typename T, typename F>
+auto start_command(command_ptr<T> & cmd, scheduler & sch, task_completion<T> & sink, F && f)
+	-> typename task_traits<decltype(f(std::declval<result<T>>()))>::task_type;
+
+template <typename T>
+result<T> dismiss_task(task<T> & t);
+
+template <typename T>
+void mark_complete(task<T> & t);
 
 }
 }
